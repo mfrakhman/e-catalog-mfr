b8baf161597ec6c6104313b39d58f3c1127d08bb
mfrakhman <rrrfajar1@gmail.com>
2023-10-19 23:51:40 +0700

    api-route CRUD for products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -7,13 +7,34 @@ const getAllProducts = async (req, res) => {
 
 const createNewProduct = async (req, res) => {
   const { name, description, price, image } = req.body;
-  const result = await knex("products").insert({
-    name,
-    description,
-    price,
-    image,
-  });
-  res.status(201).json({ name, description, price, image });
+  const result = await knex("products")
+    .insert({
+      name,
+      description,
+      price,
+      image,
+    })
+    .returning(["name", "description", "price", "image"]);
+  res.status(201).json(result);
 };
 
-module.exports = { getAllProducts, createNewProduct };
+const editProduct = async (req, res) => {
+  const { name, description, price, image } = req.body;
+  const result = await knex("products")
+    .where("id", req.params.id)
+    .update({ name, description, price, image })
+    .returning(["name", "description", "price", "image"]);
+  res.status(200).json(result);
+};
+
+const deleteProduct = async (req, res) => {
+  const result = await knex("products").where("id", req.params.id).del();
+  res.status(200).json(result);
+};
+
+module.exports = {
+  getAllProducts,
+  createNewProduct,
+  editProduct,
+  deleteProduct,
+};
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,11 +2,15 @@ const { Router } = require("express");
 const {
   getAllProducts,
   createNewProduct,
+  editProduct,
+  deleteProduct,
 } = require("../controllers/product.controller");
 
 const route = Router();
 
 route.get("/", getAllProducts);
 route.post("/", createNewProduct);
+route.patch("/:id", editProduct);
+route.delete("/:id", deleteProduct);
 
 module.exports = route;
